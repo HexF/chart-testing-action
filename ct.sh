c0987bdda782a03c96bd2e3002d9c1e18fef184c
Thomas <iamthomasnz@gmail.com>
2020-02-09 02:03:30 +1300

    Update ct.sh

--- ct.sh
+++ ct.sh
@@ -139,12 +139,12 @@ configure_kube() {
     docker_exec sh -c "mkdir -p /root/.kube;"
     docker cp "$kubeconfig" ct:/root/.kube/config
     
-    docker_exec helm repo add stable https://kubernetes-charts.storage.googleapis.com/ 
-    docker_exec helm repo update
-    docker_exec helm install stable/nfs-server-provisioner --generate-name
+    #docker_exec helm repo add stable https://kubernetes-charts.storage.googleapis.com/ 
+    #docker_exec helm repo update
+    #docker_exec helm install stable/nfs-server-provisioner --generate-name
     
     docker_exec kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false","storageclass.beta.kubernetes.io/is-default-class":"false"}}}'
-    docker_exec kubectl patch storageclass nfs -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+    docker_exec kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
     docker_exec kubectl describe storageclass
     docker_exec kubectl get storageclass
     
